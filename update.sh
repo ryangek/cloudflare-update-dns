4b7afcfbc47470d278cad647abc0d06a0083cee1
Arthit Kanjai <arthit.k@kbtg.tech>
2024-02-18 05:07:47 +0700

    update Dockerfile

--- update.sh
+++ update.sh
@@ -3,27 +3,41 @@
 # Set the API endpoint
 API_URL="https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID"
 
-prevIp=$CUR_IP
+# Store the initial public IP address
+prevIp="$CUR_IP"
+
+# Main loop
 while true; do
+    # Fetch the current public IP address
     curIp=$(curl -sS https://ipinfo.io/ip)
+
+    # Compare the current IP with the previous one
     if [ "$prevIp" != "$curIp" ]; then
-        # Make the API call
+        # Update the previous IP with the current one
         prevIp="$curIp"
+
+        # Construct JSON payload for the API request
         JSON_PAYLOAD='{
-        "content": "'"$curIp"'",
-        "name": "pinesproject.online",
-        "proxied": true,
-        "type": "A",
-        "comment": "'"$(date)"' '"$curIp"'",
-        "ttl": 3600
+            "content": "'"$curIp"'",
+            "name": "pinesproject.online",
+            "proxied": true,
+            "type": "A",
+            "comment": "'"$(date)"' '"$curIp"'",
+            "ttl": 3600
         }'
-        echo "updated ip address... $(date) $curIp"
+
+        # Display a message about the IP update
+        echo "Updated IP address at $(date): $curIp"
+
+        # Make the API call to update the DNS record
         curl --request PATCH \
-        --url "$API_URL" \
-        --header 'Content-Type: application/json' \
-        --header "X-Auth-Email: $EMAIL" \
-        --header "X-Auth-Key: $AUTH_KEY" \
-        --data "$JSON_PAYLOAD"
+             --url "$API_URL" \
+             --header 'Content-Type: application/json' \
+             --header "X-Auth-Email: $EMAIL" \
+             --header "X-Auth-Key: $AUTH_KEY" \
+             --data "$JSON_PAYLOAD"
     fi
-    sleep 300  # Sleep for 5 minutes (300 seconds)
+
+    # Sleep for 5 minutes before checking again
+    sleep 300
 done
